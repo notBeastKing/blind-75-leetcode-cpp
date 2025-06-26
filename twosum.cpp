dca810accfe79d79e6ccbd2c27df3a5fef11d121
notBeastKing <harshithmardi@gmail.com>
2025-06-26 22:48:37 +0530

    first one

---
+++ twosum.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) 
+{
+        vector<int> res;
+        int vec_len = nums.size();
+
+        for(int i = 0 ; i < vec_len-1; i++)
+        {
+            for(int j = 1; j < vec_len; j++)
+            {
+                if(nums[i] + nums[j] == target)
+                {
+                    vector<int> res = {i,j};
+                    break;
+                }
+            }
+        }
+
+        return res;
+}
+
+int main(){
+    vector<int> cases = {2, 7, 11, 15};
+
+    vector<int> ans = twoSum(cases ,9);
+
+    for(int &i : ans)
+    {
+        cout<<i;
+    }
+}
