ffcac092f9f27361220dacaa2bef40033c1bd646
notBeastKing <harshithmardi@gmail.com>
2025-06-28 01:28:18 +0530

    two sum blind 75

--- twosum.cpp
+++ twosum.cpp
@@ -1,36 +1,28 @@
 #include<stdio.h>
 #include<iostream>
 #include<vector>
+#include<unordered_map>
 
 using namespace std;
 
 vector<int> twoSum(vector<int>& nums, int target) 
-{
-        vector<int> res;
-        int vec_len = nums.size();
-
-        for(int i = 0 ; i < vec_len-1; i++)
+{   
+    unordered_map<int ,int> pairs;
+    for(int i = 0 ; i < nums.size(); i++)
+    {
+        if(pairs.find(target - nums[i]) != pairs.end())
         {
-            for(int j = 1; j < vec_len; j++)
-            {
-                if(nums[i] > target || nums[2] > target)
-                {
-                    continue;
-                }
-                else if(nums[i] + nums[j] == target)
-                {
-                    vector<int> res = {i,j};
-                    break;
-                }
-            }
+            return{(pairs[target - nums[i]]), i};
         }
+        pairs[nums[i]] = i;
+    }
 
-        return res;
+    return {};
 }
 
 int main(){
-    vector<int> cases = {2, 7, 11, 15};
-    vector<int> ans = twoSum(cases ,9);
+    vector<int> cases = {2,5,5,11};
+    vector<int> ans = twoSum(cases ,10);
 
     for(int &i : ans)
     {
